018753b5c07edc2834053bc18c90c1f96c54569e
gareththegeek <gj_williams2000@yahoo.co.uk>
2017-11-12 15:32:36 +0000

    Add support for import and export of scores column

--- features/Chess.Featuriser/Cli/Options.cs
+++ features/Chess.Featuriser/Cli/Options.cs
@@ -14,6 +14,9 @@ namespace Chess.Featuriser.Cli
         [Option('i', "input", HelpText = "Input filename - Can be one of the following: .pgn file containing one or more chess games, .csv file containing one fen per row.")]
         public string Input { get; set; }
 
+        [Option('s', "scores", HelpText = "Input scores - Indicated that the input csv contains position scores in the second column, which should be preserved in the output file as labels.")]
+        public bool Scores { get; set; }
+
         [Option('o', "output", HelpText = "Output filename - Specifies the .csv filename to which data is output. If specified either /e, /f or both must also be true. Either output filename or debug mode is required.")]
         public string Output { get; set; }
 
@@ -43,6 +46,12 @@ namespace Chess.Featuriser.Cli
             {
                 ConsoleHelper.PrintWarning($"Configuration file not found '{ConfigurationFile}'. Using command line parameters");
                 ConfigurationFile = null;
+            }
+
+            if (Path.GetExtension(Input).ToLower() == ".pgn" && Scores)
+            {
+                ConsoleHelper.PrintError("Cannot import scores from pgn file. Either specify csv input file or do not set scores parameter");
+                return false;
             }
 
             if (!Debug && string.IsNullOrEmpty(Output))
